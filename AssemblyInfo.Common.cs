e405cb15d963fa5738820a296978d81731641fa6
Gilles TOURREAU <gilles.tourreau@pos-informatique.fr>
2017-06-13 15:43:18 +0200

    Add files for NuGet packaging and a Build.proj which allows to publish the libraru on NuGet website.

--- AssemblyInfo.Common.cs
+++ AssemblyInfo.Common.cs
@@ -8,11 +8,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("P.O.S Informatique")]
-[assembly: AssemblyCopyright("Copyright © 1999-2017 - All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 1999-2017 P.O.S Informatique - All rights reserved.")]
 [assembly: AssemblyCulture("en")]
 [assembly: AssemblyProduct("P.O.S Informatique® Framework")]
 
 [assembly: ComVisible(false)]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AssemblyInfo.Version.cs
@@ -0,0 +1,9 @@
+//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.Version.cs" company="P.O.S Informatique">
+// Copyright (c) P.O.S Informatique. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.0")]
